b7e3398500f4fc47872ed34e0b77d48434043f2e
Pedro Sánchez Casas <pedro.s.casas@tokiota.com>
2018-08-07 12:15:23 +0200

    wire redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/'
 
@@ -6,6 +6,19 @@ const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
+let store
+
+if (process.env.NODE_ENV === 'production') {
+    store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    )
+}
 
 export default store
